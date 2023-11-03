6b06128c4c57b25344819c031bd41ffa35dece7e
Roman Barinov <rbarinov2004@yahoo.com>
2023-11-03 00:50:48 -0400

    checkpoint 1

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,32 +3,36 @@ import json
 # import related models here
 from requests.auth import HTTPBasicAuth
 
+URL_GET_DEALERS = "https://us-south.functions.appdomain.cloud/api/v1/web/4e4780ac-8a49-40a5-afa4-6f38d2228df1/dealership-package/get-dealership"
 
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(params):
+    response = requests.get(url, params=params, headers={'Content-Type': 'application/json'})
+        #auth=HTTPBasicAuth('apikey', api_key))
+    return response.body
 
 
-# Create a `post_request` to make HTTP POST requests
-# e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(kwargs, api_key, payload):
+    response = requests.post(url, params=kwargs, json=payload)
+    return response.status
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
-
+#def get_dealers_from_cf(url, **kwargs):
+def get_dealers_from_cf():
+    result = get_request(url=URL_GET_DEALERS)
+    return result
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-# def get_dealer_by_id_from_cf(url, dealerId):
+def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
-
+    pass
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
+def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+    pass
 
 
--- server/djangoapp/tests.py
+++ server/djangoapp/tests.py
@@ -1,3 +1,16 @@
+import unittest
 from django.test import TestCase
+from restapis import get_dealers_from_cf
 
 # Create your tests here.
+class Testing(TestCase):
+    def test_get_dealers_from_cf(self):
+        result = get_dealers_from_cf()
+        print(result)
+        self.assertEqual(1, 1)
+
+    def test_get_dealers_from_cf(self):
+        self.assertEqual(2, 2)
+
+if __name__ == '__main__':
+    unittest.main()
